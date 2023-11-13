9c75a04650c194728c89573e555e3b2e78f17e65
Thalles Rafael <thallesrafaell@outlook.com>
2023-11-13 02:48:00 -0300

    DEBUG IMAGEM

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
 
 import style from './Header.module.css'
+import Imagem from '../assets/imgcalc.png'
+
 const Header = () => {
     return (
         <div className={style.header}>
         <h1 className={style.title}>Calculadora IMC</h1>
-        <img src="./src/assets/imgcalc.png" alt="" />
+        <img src={Imagem} alt="" />
         </div>
     )
 }
